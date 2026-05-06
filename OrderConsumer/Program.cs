7c6b98870e5b8d6f78541c32bea2f1f3180ccd1c
Juliano Galhardo <jubagalhardo9@gmail.com>
2026-05-06 15:10:01 -0300

    feat: implement OrderProducer with basic queue declaration

--- OrderConsumer/Program.cs
+++ OrderConsumer/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = await factory.CreateConnectionAsync();
+
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "order_queue", 
+                                durable: false, 
+                                exclusive: false, 
+                                autoDelete: false, 
+                                arguments: null);
+
+string message = "Pedido #123: Smartphone Apple M3";
+var body = Encoding.UTF8.GetBytes(message);
+
+// 4. Publicação usando o novo padrão
+await channel.BasicPublishAsync(exchange: string.Empty, 
+                                routingKey: "order_queue", 
+                                body: body);
+
+Console.WriteLine($" [x] Enviado: {message}");
